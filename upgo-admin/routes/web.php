fdcdb9aa72102ec5dd47c7660e0e0e9c170df7f3
Alex.xiang <xiangbohua@ruigushop.com>
2024-04-11 00:45:24 +0800

    测试

--- upgo-admin/routes/web.php
+++ upgo-admin/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'HomeController@index');
