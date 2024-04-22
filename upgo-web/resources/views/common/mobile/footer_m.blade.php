513b37557cac7c51fbe2025c896b7a17773dd261
Alex.xiang <xiangbohua@ruigushop.com>
2024-04-22 21:25:58 +0800

    开始构造移动版本

---
+++ upgo-web/resources/views/common/mobile/footer_m.blade.php
@@ -0,0 +1,54 @@
+<div id="footer">
+    <div class="module" id="flinks">
+        <div class="module_container">
+            <ul class="content_list row gutter">
+                <li id="fitem_block_0" class="fitem_block_info item_block col-50 wow">
+                    <h3 class="title">
+                        <a href="">{{$brand_title}}</a>
+                    </h3>
+                    <p class="description" style="height: 120px">{{$brand_desc}}</p>
+                    <div class="shareicon">
+
+                    </div>
+                </li>
+                <li id="fitem_block_1" class="fitem_block_link item_block col-25 wow">
+                    <ul>
+                        <li class="title"><span class="ellipsis">上和行upgo</span></li>
+                        <li><a class="ellipsis" href="case/cate/0/page/0" target="_blank">案例</a></li>
+                        <li><a class="ellipsis" href="service" target="_blank">服务范围</a></li>
+                        <li><a class="ellipsis" href="about" target="_blank">关于上和行</a></li>
+                    </ul>
+                </li>
+{{--                <li id="fitem_block_2" class="fitem_block_link item_block col-25 wow">--}}
+{{--                    <ul>--}}
+{{--                        <li class="title"><span class="ellipsis">服务范围</span></li>--}}
+{{--                        @foreach($services as $ser)--}}
+{{--                            <li><a class="ellipsis" href="{{hServiceDetail($ser->serviceId)}}" target="_blank">{{$ser->title}}</a></li>--}}
+{{--                        @endforeach--}}
+{{--                    </ul>--}}
+{{--                </li>--}}
+
+                <li id="fitem_block_3" class="fitem_block_link item_block col-25 wow">
+                    <ul>
+                        <li class="title"><span class="ellipsis">联系地址</span></li>
+                        @foreach($addressList as $address)
+                            <li><a class="ellipsis" href="{{$address->naviUrl}}" target="_blank">{{$address->detailAddress}}</a></li>
+                        @endforeach
+                        @if(sizeof($addressList) > 3)
+                            <li><a class="ellipsis" href="page/contact" target="_blank">更多联系地址</a></li>
+                        @endif
+                        <li><a class="ellipsis">联系微信：{{$business_wechat}}</a></li>
+                        <li><a class="ellipsis">联系电话：{{$resume_contact}}</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <div class="module" id="copyright">
+        <div class="module_container">
+            <span class="ellipsis">COPYRIGHT (©) 2024  {{$slogan}} </span>
+            <a id="fbeian" href="https://beian.miit.gov.cn/" target="_blank">{{$site_code}}</a>
+            <span style="vertical-align: top;">&nbsp;&nbsp;&nbsp;&nbsp;技术支持</span>
+        </div>
+    </div>
+</div>
---
+++ upgo-web/resources/views/common/mobile/navi_m.blade.php
@@ -0,0 +1,60 @@
+<div id="header">
+    <div class="content">
+        <div id="headTop">
+            <a href="/" id="logo"><img src="{{hUrlImage($site_logo)}}" alt="{{$brand_title}}" /></a>
+        </div>
+        <div id="navWrapper">
+            <div class="content">
+                <ul class="nav">
+                    <li class="navitem">
+                        <a  class="navitem" href="/" target="_self"><span data-title="首页">首页</span>
+                        </a>
+                    </li>
+                    <li class="navitem">
+                        <a  href="{{hCategoryPage(0)}}" target="_self"><span data-title="案例">案例</span>
+                        </a>
+                    </li>
+                    <li class="navitem">
+                        <a  href="/service" target="_self"><span data-title="服务范围">服务范围</span>
+                        </a>
+                    </li>
+                    <li class="navitem">
+                        <a  href="/about" target="_self"><span data-title="关于">关于{{$brand_short_name}}</span>
+                        </a>
+                    </li>
+{{--                    <li class="navitem">--}}
+{{--                        <a  href="/partner" target="_self"><span data-title="合作伙伴">合作伙伴</span>--}}
+{{--                        </a>--}}
+{{--                    </li>--}}
+{{--                    <li class="navitem">--}}
+{{--                        <a  href="/contact" target="_self"><span data-title="联系我们">联系我们</span>--}}
+{{--                        </a>--}}
+{{--                    </li>--}}
+                </ul>
+                <div id="openBtn" class="fl btn">
+                    <div class="lcbody">
+                        <div class="lcitem top">
+                            <div class="rect top"></div>
+                        </div>
+                        <div id="openMenu" class="lcitem bottom">
+                            <div class="rect bottom"></div>
+                        </div>
+                    </div>
+                </div>
+{{--                <div class="search-btn"><a href="javascript:;" class="header-search-form"><i class="fa fa-search search-button"></i></a></div>--}}
+{{--                <div id="searchbar" class="search-wrap">--}}
+{{--                    <div class="search-frame clearfix">--}}
+{{--                        <div class="clearfix " action="search">--}}
+{{--                            <a href="javascript:;" class="fl searchtext-btn"><i class="fa fa-search search-button"></i></a>--}}
+{{--                            <div class="search-input">--}}
+{{--                                <input placeholder="搜索" name="s"  value="" autocomplete="off" type="text" class=" fl">--}}
+{{--                            </div>--}}
+{{--                        </div>--}}
+{{--                        <a href="javascript:;" class="search-close"><i class="lcitem-top"></i><i class="lcitem-bottom"></i></a>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+                <div class="clear"></div>
+            </div>
+        </div>
+    </div>
+</div>
