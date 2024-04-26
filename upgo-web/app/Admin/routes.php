19f34b9c9951b384998b08e11b083e005328c6de
Alax.xiang <xiangbohua@ruigushop.com>
2024-04-26 13:57:33 +0800

    modify url

--- upgo-web/app/Admin/routes.php
+++ upgo-web/app/Admin/routes.php
@@ -25,7 +25,7 @@ Route::group([
     $router->resource('address', 'AddressAdminController');
     $router->resource('category', 'CategoryAdminController');
     $router->resource('about', 'AboutAdminController');
-    $router->resource('page', 'PageAdminController');
+    $router->resource('pages', 'PageAdminController');
 
     Route::get('/case/detail/{caseId}', 'CaseAdminController@editImages');
     Route::get('/service/detail/{serviceId}', 'CaseAdminController@editImages');
