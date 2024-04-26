ff13ad24958db4082c8cbdfe96ff200c1985004f
Alex.xiang <xiangbohua@ruigushop.com>
2024-04-26 23:34:31 +0800

    fix

--- upgo-web/resources/views/mobile/page.blade.php
+++ upgo-web/resources/views/mobile/page.blade.php
@@ -34,12 +34,12 @@
                                                             <p style="font-size: 18px; line-height: normal; padding-bottom: 10px; padding-top: 10px">{{$detail->detail_title}}</p>
                                                             <p>{{$detail->detail_desc}}</p>
                                                         @endif
-
-                                                        @if(!empty($detail->image_url))
-                                                            <p>
+                                                        <p>
+                                                            @if(!empty($detail->image_url))
                                                                 <img alt="" style="width: 100%" src="{{hUrlImage($detail->image_url)}}"/>
-                                                            </p>
-                                                        @endif
+                                                            @endif
+                                                        </p>
+
                                                         @if($detail->text_position == 0)
                                                             <p style="font-size: 18px; line-height: normal; padding-bottom: 10px; padding-top: 10px">{{$detail->detail_title}}</p>
                                                             <p>{{$detail->detail_desc}}</p>
--- upgo-web/resources/views/page.blade.php
+++ upgo-web/resources/views/page.blade.php
@@ -34,16 +34,15 @@
                                                             <p style="font-size: 38px; line-height: normal; padding-bottom: 20px; padding-top: 10px">{{$detail->detail_title}}</p>
                                                             <p style="font-size: 18px; line-height: normal; padding-bottom: 20px">{{$detail->detail_desc}}</p>
                                                         @endif
-
-                                                        @if(!empty($detail->image_url))
-                                                            <p><img alt="" style="width: 100%" src="{{hUrlImage($detail->image_url)}}"></p>
-                                                        @endif
-
+                                                        <p>
+                                                            @if(!empty($detail->image_url))
+                                                                <img alt="" style="width: 100%" src="{{hUrlImage($detail->image_url)}}">
+                                                            @endif
+                                                        </p>
                                                         @if($detail->text_position == 0)
                                                             <p style="font-size: 38px; line-height: normal; padding-bottom: 20px; padding-top: 10px">{{$detail->detail_title}}</p>
                                                             <p style="font-size: 18px; line-height: normal; padding-bottom: 20px">{{$detail->detail_desc}}</p>
                                                         @endif
-
                                                      @endforeach
                                                 </div>
                                             </div>
