7992ace175e6b83d2dc9e25784a6598cdf321dd4
Alex.xiang <xiangbohua@ruigushop.com>
2024-06-20 11:33:23 +0800

    页面选择问题

--- upgo-web/app/Service/WebPageService.php
+++ upgo-web/app/Service/WebPageService.php
@@ -7,23 +7,23 @@ use Illuminate\Support\Facades\DB;
 
 class WebPageService
 {
-   public function getPageTitle($pageId) {
-       $title = DB::table('web_page')->where('id', $pageId)->value('page_title');
+   public function getPageName($pageId) {
+       $title = DB::table('web_page')->where('id', $pageId)->value('page_name');
        if (empty($title)) {
            return '';
        }
        return $title;
    }
 
-   public function listAllPage($pageType = 0) {
-       $query = DB::table('web_page')->select('id', 'page_title');
+   public function listAllPage($pageType = 2) {
+       $query = DB::table('web_page')->select('id', 'page_name');
         if (!empty($pageType)) {
             $query->where('page_type', $pageType);
         }
        $pages = $query->get();
        $result = [];
        foreach ($pages as $page) {
-           $result[$page->id] = $page->page_title;
+           $result[$page->id] = $page->page_name;
        }
        return $result;
    }
